577520fc5091c5595643a665293387f74d4592ef
Hugh <hieu.tranminhhcm@gmail.com>
2019-03-14 10:08:25 +0700

    create refs branch

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,25 +2,16 @@
   <input
     type="text"
     class="input-text"
-    v-model="content"
-    placeholder="What need to be done?" @keypress.exact.prevent.enter="addData">
+    ref="add"
+    placeholder="What need to be done?" @keyup.enter="addData">
 </template>
 <script>
 export default {
-  data () {
-    return {
-      content: ''
-    }
-  },
   methods: {
     addData (event) {
-      let self = this
-      if (this.content) {
-        this.$store.dispatch('ADD_DATA', this.content).then(function () {
-          self.content = ''
-        })
-        console.log('--keyup ', event.target.value)
-      }
+      this.$store.dispatch('ADD_DATA', event.target.value)
+      this.$refs.add.value = ''
+      console.log('--keyup ', event.target.value)
     }
   }
 }
